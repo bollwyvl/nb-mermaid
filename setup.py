04f2cef25762aff729149d736726c9ac4fca4266
Nicholas Bollweg <nick.bollweg@gmail.com>
2015-07-26 00:33:27 -0400

    starting python module

---
+++ setup.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import json
+import sys
+
+from setuptools import setup
+
+
+try:
+    from jupyterpip import cmdclass
+except:
+    import pip
+    import importlib
+
+    pip.main(["install", "jupyter-pip"])
+    cmdclass = importlib.import_module("jupyterpip").cmdclass
+
+
+with open("setup.json") as f:
+    setup_data = json.load(f)
+
+if sys.version_info[0] < 3:
+    # insidious! http://bugs.python.org/issue13943
+    setup_data["packages"] = [s.encode("utf-8")
+                              for s in setup_data["packages"]]
+
+with open("README.rst") as f:
+    setup_data.update(
+        long_description=f.read()
+    )
+
+setup_data.update(
+    cmdclass=cmdclass(
+        path="{0}/static/{0}".format(
+            setup_data["packages"][0]
+        )
+    )
+)
+
+setup(**setup_data)
